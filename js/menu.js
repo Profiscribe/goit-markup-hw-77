f5efcddc739df2041063068a2c9c92446b2fc0f1
Оксана Качмар <108275501+Profiscribe@users.noreply.github.com>
2022-08-02 19:22:15 +0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector(".open-btn"),
-    closeMenuBtn: document.querySelector(".close-btn"),
-    menu: document.querySelector(".mob-menu"),
+    openMenuBtn: document.querySelector('.open-btn'),
+    closeMenuBtn: document.querySelector('.close-btn'),
+    menu: document.querySelector('.mob-menu'),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleModal() {
+  function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
   }
 })();
